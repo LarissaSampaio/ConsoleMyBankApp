c64f3597c7dd21d2c42fb2b8d17ccf14864dc1d6
LarissaSampaio <larisampaio@hotmail.com>
2021-10-15 20:49:09 -0300

    Commit 15.10

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -8,15 +8,37 @@ namespace ConsoleMyBankApp
         {
             Cliente cliente = new Cliente("Larissa Sampaio", "000.000.000-00", "Desenvolvedora");
 
-            ContaCorrente conta = new ContaCorrente();
+            ContaCorrente conta = new ContaCorrente(cliente, 01, 0001);
 
-            conta.titular = cliente;
-            conta.agencia = 01;
-            conta.conta = 0001;
 
-            Console.WriteLine("Titular: {0}", conta.titular.nome);
-            Console.WriteLine("Agência: {0}", conta.agencia);
-            Console.WriteLine("Conta Corrente: {0}", conta.conta);
+            Console.WriteLine("Conta Corrente Criada");
+            Console.WriteLine("Titular: {0}", conta.getTitular().getNome());
+            Console.WriteLine("Agência: {0}", conta.getAgencia());
+            Console.WriteLine("Conta Corrente: {0}", conta.getConta());
+
+            Console.WriteLine("Parabéns! Seu saldo é de R${0}, obrigada por criar sua conta!", conta.getSaldo());
+
+            double valorDeposito = 500;
+            conta.Depositar(valorDeposito);
+            
+            Console.WriteLine("Seu saldo é de R${0}.", conta.getSaldo());
+
+            double valorSaque = 100;
+            conta.Sacar(valorSaque);
+
+            Console.WriteLine("Seu saldo é de R${0}.", conta.getSaldo());
+            
+            Cliente clienteSilvia = new Cliente("Tiago Ávila", "000.000.000-00", "Estudante");
+
+            ContaCorrente contaSilvia = new ContaCorrente(clienteSilvia, 01, 0002);
+
+            double valorPix = 500;
+            conta.Pix(valorPix, contaSilvia);
+
+            Console.WriteLine("Seu saldo é de R${0}.", conta.getSaldo());
+            Console.WriteLine("Seu saldo é de R${0}.", contaSilvia.getSaldo());
+
+
         }
     }
 }
