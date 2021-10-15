16891f1b6e3c29f3a88f5dfda9c780f64cd56cdb
LarissaSampaio <larisampaio@hotmail.com>
2021-10-14 21:42:42 -0300

    Classe Conta e Cliente Criadas

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,7 +6,21 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente();
+
+            cliente.nome = "Larissa Sampaio";
+            cliente.cpf = "000.000.000-00";
+            cliente.profissao = "Desenvolvedora";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = cliente;
+            conta.agencia = 01;
+            conta.conta = 0001;
+
+            Console.WriteLine("Titular: {0}", conta.titular);
+            Console.WriteLine("Agência: {0}", conta.agencia);
+            Console.WriteLine("Conta Corrente: {0}", conta.conta);
         }
     }
 }
